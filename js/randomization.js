29e8fb51d6db78b6469e912a589d973747e46c7c
Ritika Rajendra Rawat <106914327+Rita1791@users.noreply.github.com>
2026-01-05 00:18:06 +0530

    Update randomization.js

--- js/randomization.js
+++ js/randomization.js
@@ -1,5 +1,7 @@
-function randomizeProduct(previous=null) {
-  const products = ["A19","K87"];
-  if (!previous) return products[Math.floor(Math.random()*2)];
+function randomizeProduct(previous = null) {
+  const products = ["A19", "K87"];
+  if (!previous) {
+    return products[Math.floor(Math.random() * 2)];
+  }
   return products.find(p => p !== previous);
 }

3fb559d03ba36dd4ff1443d90a263671f8b49ec8
Ritika Rajendra Rawat <106914327+Rita1791@users.noreply.github.com>
2026-01-05 18:34:35 +0530

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,60 +1,18 @@
-// js/db.js
+request.onupgradeneeded = function (e) {
+  db = e.target.result;
 
-let db = null;
+  if (!db.objectStoreNames.contains("patients"))
+    db.createObjectStore("patients", { keyPath: "patientId" });
 
-function openDB() {
-  return new Promise((resolve, reject) => {
-    const request = indexedDB.open("clinelphDB", 1);
+  if (!db.objectStoreNames.contains("visits"))
+    db.createObjectStore("visits", { keyPath: "id", autoIncrement: true });
 
-    request.onupgradeneeded = function (e) {
-      db = e.target.result;
+  if (!db.objectStoreNames.contains("adverseEvents"))
+    db.createObjectStore("adverseEvents", { keyPath: "id", autoIncrement: true });
 
-      if (!db.objectStoreNames.contains("patients")) {
-        db.createObjectStore("patients", { keyPath: "patientId" });
-      }
+  if (!db.objectStoreNames.contains("withdrawals"))
+    db.createObjectStore("withdrawals", { keyPath: "id", autoIncrement: true });
 
-      if (!db.objectStoreNames.contains("visits")) {
-        db.createObjectStore("visits", { keyPath: "id", autoIncrement: true });
-      }
-
-      if (!db.objectStoreNames.contains("auditLogs")) {
-        db.createObjectStore("auditLogs", { keyPath: "id", autoIncrement: true });
-      }
-    };
-
-    request.onsuccess = function (e) {
-      db = e.target.result;
-      resolve(db);
-    };
-
-    request.onerror = function () {
-      reject("Failed to open IndexedDB");
-    };
-  });
-}
-
-/* ---------- GENERIC HELPERS ---------- */
-
-function addRecord(storeName, data) {
-  return new Promise(resolve => {
-    const tx = db.transaction(storeName, "readwrite");
-    tx.objectStore(storeName).add(data);
-    tx.oncomplete = () => resolve();
-  });
-}
-
-function getAllRecords(storeName) {
-  return new Promise(resolve => {
-    const tx = db.transaction(storeName, "readonly");
-    const req = tx.objectStore(storeName).getAll();
-    req.onsuccess = () => resolve(req.result);
-  });
-}
-
-function updateRecord(storeName, data) {
-  return new Promise(resolve => {
-    const tx = db.transaction(storeName, "readwrite");
-    tx.objectStore(storeName).put(data);
-    tx.oncomplete = () => resolve();
-  });
-}
+  if (!db.objectStoreNames.contains("events"))
+    db.createObjectStore("events", { keyPath: "id", autoIncrement: true });
+};

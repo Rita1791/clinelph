3d2a772d70e967308b5b319a96cbd3ec20646913
Ritika Rajendra Rawat <106914327+Rita1791@users.noreply.github.com>
2026-01-05 18:21:50 +0530

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,33 +1,60 @@
-let db;
+// js/db.js
+
+let db = null;
 
 function openDB() {
-  return new Promise(resolve => {
-    const req = indexedDB.open("clinelphDB", 1);
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open("clinelphDB", 1);
 
-    req.onupgradeneeded = e => {
+    request.onupgradeneeded = function (e) {
       db = e.target.result;
-      db.createObjectStore("patients", { keyPath: "patientId" });
-      db.createObjectStore("visits", { keyPath: "id", autoIncrement: true });
+
+      if (!db.objectStoreNames.contains("patients")) {
+        db.createObjectStore("patients", { keyPath: "patientId" });
+      }
+
+      if (!db.objectStoreNames.contains("visits")) {
+        db.createObjectStore("visits", { keyPath: "id", autoIncrement: true });
+      }
+
+      if (!db.objectStoreNames.contains("auditLogs")) {
+        db.createObjectStore("auditLogs", { keyPath: "id", autoIncrement: true });
+      }
     };
 
-    req.onsuccess = e => {
+    request.onsuccess = function (e) {
       db = e.target.result;
-      resolve();
+      resolve(db);
+    };
+
+    request.onerror = function () {
+      reject("Failed to open IndexedDB");
     };
   });
 }
 
-function add(store, data) {
-  db.transaction(store, "readwrite").objectStore(store).add(data);
+/* ---------- GENERIC HELPERS ---------- */
+
+function addRecord(storeName, data) {
+  return new Promise(resolve => {
+    const tx = db.transaction(storeName, "readwrite");
+    tx.objectStore(storeName).add(data);
+    tx.oncomplete = () => resolve();
+  });
 }
 
-function getAll(store) {
+function getAllRecords(storeName) {
   return new Promise(resolve => {
-    const req = db.transaction(store).objectStore(store).getAll();
+    const tx = db.transaction(storeName, "readonly");
+    const req = tx.objectStore(storeName).getAll();
     req.onsuccess = () => resolve(req.result);
   });
 }
 
-function update(store, data) {
-  db.transaction(store, "readwrite").objectStore(store).put(data);
+function updateRecord(storeName, data) {
+  return new Promise(resolve => {
+    const tx = db.transaction(storeName, "readwrite");
+    tx.objectStore(storeName).put(data);
+    tx.oncomplete = () => resolve();
+  });
 }

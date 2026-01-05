cbe400a4c993a4d617b70327666db6782474c2b0
Ritika Rajendra Rawat <106914327+Rita1791@users.noreply.github.com>
2026-01-05 18:49:45 +0530

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,18 +1,43 @@
-request.onupgradeneeded = function (e) {
-  db = e.target.result;
+let db;
 
-  if (!db.objectStoreNames.contains("patients"))
-    db.createObjectStore("patients", { keyPath: "patientId" });
+function openDB() {
+  return new Promise((resolve) => {
+    const req = indexedDB.open("clinelphDB", 1);
+    req.onupgradeneeded = e => {
+      db = e.target.result;
+      db.createObjectStore("patients", { keyPath: "patientId" });
+      db.createObjectStore("visits", { keyPath: "id", autoIncrement: true });
+      db.createObjectStore("adverseEvents", { keyPath: "id", autoIncrement: true });
+      db.createObjectStore("withdrawals", { keyPath: "id", autoIncrement: true });
+      db.createObjectStore("auditLogs", { keyPath: "id", autoIncrement: true });
+    };
+    req.onsuccess = e => {
+      db = e.target.result;
+      resolve();
+    };
+  });
+}
 
-  if (!db.objectStoreNames.contains("visits"))
-    db.createObjectStore("visits", { keyPath: "id", autoIncrement: true });
+function addRecord(store, data) {
+  return new Promise(res => {
+    const tx = db.transaction(store, "readwrite");
+    tx.objectStore(store).add(data);
+    tx.oncomplete = () => res();
+  });
+}
 
-  if (!db.objectStoreNames.contains("adverseEvents"))
-    db.createObjectStore("adverseEvents", { keyPath: "id", autoIncrement: true });
+function getAllRecords(store) {
+  return new Promise(res => {
+    const tx = db.transaction(store, "readonly");
+    const req = tx.objectStore(store).getAll();
+    req.onsuccess = () => res(req.result);
+  });
+}
 
-  if (!db.objectStoreNames.contains("withdrawals"))
-    db.createObjectStore("withdrawals", { keyPath: "id", autoIncrement: true });
-
-  if (!db.objectStoreNames.contains("events"))
-    db.createObjectStore("events", { keyPath: "id", autoIncrement: true });
-};
+function updateRecord(store, data) {
+  return new Promise(res => {
+    const tx = db.transaction(store, "readwrite");
+    tx.objectStore(store).put(data);
+    tx.oncomplete = () => res();
+  });
+}
